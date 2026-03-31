eea9e1d80c408a78f62b3179088f7e58a046c07a
Timothy Moxham <timothymoxham@outlook.com>
2026-03-31 21:52:36 +1000

    Implement getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello World")
+
+function getComputerChoice() {
+    let computerChoice = Math.random();
+    if (computerChoice <= (1 / 3)) {
+        return "rock";
+    }
+    else if (computerChoice <= 2/3) {
+        return "scissors";
+    }
+    else {
+        return "paper";
+    }
+}
