6e2fbc3fd11fa130bd76c20ba367d2d07984999a
Timothy Moxham <timothymoxham@outlook.com>
2026-04-01 17:11:46 +1000

    Implement getHumanChoice and initial values for humanScore and computerScore

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 console.log("Hello World")
 
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
     let computerChoice = Math.random();
     if (computerChoice <= (1 / 3)) {
         return "rock";
-    }
-    else if (computerChoice <= 2/3) {
+    } else if (computerChoice <= 2 / 3) {
         return "scissors";
-    }
-    else {
+    } else {
         return "paper";
     }
 }
+
+function getHumanChoice() {
+    return prompt("rock, paper, scissors: ");
+}
+
+console.log(getHumanChoice())
